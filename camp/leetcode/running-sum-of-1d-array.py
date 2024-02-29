dd25694756f64760956212574b69bb35dbf116ca
dura12 <124529996+dura12@users.noreply.github.com>
2024-02-29 09:12:04 +0300

    Add solution for Running Sum of 1d Array

---
+++ camp/leetcode/running-sum-of-1d-array.py
@@ -0,0 +1,15 @@
+# class Solution:
+#     def runningSum(self, nums: List[int]) -> List[int]:
+#         result=[0]*len(nums)
+#         result[0]=nums[0]
+#         for i in range(1,len(nums)):
+#             result[i]=nums[i-1]+nums[i]
+#         return result
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = [0] * len(nums)
+        result[0] = nums[0]
+        for i in range(1, len(nums)):
+            result[i] = result[i - 1] + nums[i]
+        return result
+
