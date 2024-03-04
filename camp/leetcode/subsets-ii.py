f90e069fc79ef46d63813b8ea0db6d4466f597af
dura12 <124529996+dura12@users.noreply.github.com>
2024-03-04 18:59:02 +0300

    Add solution for Subsets II

---
+++ camp/leetcode/subsets-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        ans = []
+        def back(i , curr):
+            if i <= len(nums):
+                temp = curr.copy()
+                temp.sort()
+                if temp not in res:
+                    res.append(temp.copy())
+            else:
+                return
+            for j in range(i , len(nums)):
+                curr.append(nums[j])
+                back(j + 1 , curr)
+                curr.pop()
+
+        back(0 , [])
+        return res
