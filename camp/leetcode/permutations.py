aae6fe7fff78c2a2fec58835c722601cbb8e4cf3
dura12 <124529996+dura12@users.noreply.github.com>
2024-03-05 12:47:26 +0300

    Add solution for Permutations

---
+++ camp/leetcode/permutations.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        res = []
+        def back(curr):
+            if len(set(curr)) == len(nums):
+                res.append(curr.copy())
+                return 
+            for i in nums:
+                if i in curr:
+                    continue
+                curr.append(i)
+                back(curr)
+                curr.pop()
+        back([])
+        return res
