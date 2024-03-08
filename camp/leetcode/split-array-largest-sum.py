685472239a6e51650e83319cfad84081ad0e3f4a
dura12 <124529996+dura12@users.noreply.github.com>
2024-03-08 18:58:41 +0300

    Add solution for Split Array Largest Sum

---
+++ camp/leetcode/split-array-largest-sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def check(arr, guess, k):
+            s = 0
+            c = 1
+            for i in arr:
+                if s + i > guess:
+                    s = 0
+                    c += 1
+                s += i
+            return c > k
+                
+
+        left = max(nums)
+        right = sum(nums)
+
+        while left <= right:
+            mid = (left + right)//2
+            if check(nums, mid, k):
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
+
+
+
+
